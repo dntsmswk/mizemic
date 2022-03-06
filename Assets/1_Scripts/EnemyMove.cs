3ad1f3fee6a5c3ac73c9a9cc8285000022f5c9c1
Leedoyeon5 <ldy040823@gmail.com>
2022-03-06 15:25:50 +0900

    몬스터 house접근시 회전중지

--- Assets/1_Scripts/EnemyMove.cs
+++ Assets/1_Scripts/EnemyMove.cs
@@ -4,8 +4,10 @@ using UnityEngine;
 
 public class EnemyMove : MonoBehaviour
 {
-    delegate void playerTR(GameObject ptr);
-    playerTR playerTr;
+    delegate void PlayerTR(GameObject ptr);
+    PlayerTR playerTr;
+
+    RaycastHit2D layHit;
 
     
     [SerializeField]
@@ -16,6 +18,7 @@ public class EnemyMove : MonoBehaviour
     public enum State
     {
         MOVE,
+        TURNOVER,
         DIE,
         UP,
         DOWN,
@@ -27,6 +30,7 @@ public class EnemyMove : MonoBehaviour
     void Start()
     {
         isDie = false;
+
         state = State.MOVE;
         //델리게이로 만든 변수 안에 함수 넣어
         playerTr = Enemy_MOVE;
@@ -37,14 +41,34 @@ public class EnemyMove : MonoBehaviour
         Vector2 vec = new Vector2(transform.position.x - playerTr.transform.position.x,
                                   transform.position.y - playerTr.transform.position.y);
         float angle = Mathf.Atan2(vec.y, vec.x) * Mathf.Rad2Deg;
+
+        //state가 TURNOVER로 바뀌면 회전을 정지
+        if (!(state == State.TURNOVER))
         transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
 
         transform.Translate(Vector3.left * moveSpeed * Time.deltaTime);
+    }
+    public void Enemy_TURNOVER()
+    {
+        //ray를 시각적으로 표현
+        Debug.DrawRay(transform.position, -transform.right * 5, Color.green);
+
+
+        //광선이 내가 지정한 레이어의 닿았다면 턴을 종료
+        if (Physics2D.Raycast(transform.position,
+                           -transform.right,
+                           1,
+                           1 << 6))
+        {
+            state = State.TURNOVER;
+            Debug.Log("house맞.");
+        }
     }
    
     void Update()
     {
         // 이야~~ 델리게이트 쓰는 코드가 깔 끔 하구먼 
-            playerTr(GameObject.Find("player"));
+        playerTr(GameObject.Find("player"));
+        Enemy_TURNOVER();
     }
 }
