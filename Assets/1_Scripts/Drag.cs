14c97ef8e62339b917b47a892c746c766860d272
Leedoyeon5 <ldy040823@gmail.com>
2022-04-27 22:32:22 +0900

    바뀐 블록 디자인 적용

--- Assets/1_Scripts/Drag.cs
+++ Assets/1_Scripts/Drag.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class Drag : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler, IDropHandler
 {
@@ -13,6 +14,10 @@ public class Drag : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHand
     
     public float dState;
 
+    public Image[] image;
+    Color[] color;
+
+
     public enum directionState
     {
         UP,
@@ -20,6 +25,17 @@ public class Drag : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHand
         RIGHT,
         LEFT
     }
+
+    void Start()
+    {
+        startPoint = transform.position;
+       // for (int i = 0; i < 4; i++)
+       // {
+        //    image[i] = image[i].gameObject.GetComponent<Image>();
+       //     color[i] = image[i].color;
+        //}
+    }
+
 
     public void OnBeginDrag(PointerEventData eventData)
     {
@@ -32,28 +48,46 @@ public class Drag : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHand
         {
             dState = 1;
             print(dState);
+            //// 선택중인 블록은 반투명하게 함
+            //color[0].a = 0.5f;
+            //image[0].color = color[0];
         }
            
         if (direcState == directionState.UP)
         {
             dState = 2;
             print(dState);
+            //// 선택중인 블록은 반투명하게 함
+            //color[1].a = 0.5f;
+            //image[1].color = color[1];
         }
         if (direcState == directionState.RIGHT)
         {
             dState = 3;
             print(dState);
+            //// 선택중인 블록은 반투명하게 함
+            //color[2].a = 0.5f;
+            //image[2].color = color[2];
         }
         if (direcState == directionState.LEFT)
         {
             dState = 4;
             print(dState);
+            //// 선택중인 블록은 반투명하게 함
+            //color[3].a = 0.5f;
+            //image[3].color = color[3];
         }
         transform.position = eventData.position;
     }
 
     public void OnEndDrag(PointerEventData eventData)
     {
+        //for (int i = 0; i < 4; i++)
+        //{
+        //    color[i].a = 1f;
+        //    image[i].color = color[i];
+        //}
+        
         transform.position = startPoint;
     }
   
@@ -61,13 +95,5 @@ public class Drag : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHand
     public void OnDrop(PointerEventData eventData)
     {
     }
-    void Start()
-    {
-        startPoint = transform.position;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-    }
+    
 }
